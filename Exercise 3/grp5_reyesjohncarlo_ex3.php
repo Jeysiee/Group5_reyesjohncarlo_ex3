5c27e8972ee849236beca9df97eb099ac883e46a
John Carlo Reyes <johncarloreyes018@gmail.com>
2024-09-12 16:34:36 +0800

    Add files via upload

--- Exercise 3/grp5_reyesjohncarlo_ex3.php
+++ Exercise 3/grp5_reyesjohncarlo_ex3.php
@@ -1,27 +1,49 @@
 <?php
 $fileName = 'sample.txt';
+$fileContents = file_get_contents($fileName);
+
 
 if (file_exists($fileName)) {
-    echo "John Carlo\n";
+    echo "File opened.<br>";
     
-    $fileContents = file_get_contents($fileName);
-    echo "BSIT-3G:\n";
-    echo $fileContents . "\n";  
+    
+ file_get_contents($fileName);
+    if ($fileContents === false) {
+        echo "Error reading the file.\n";
+    } else {
+        echo "File contents:\n";
+        echo $fileContents . "<br>";  
 
-    $lines = file($fileName, FILE_IGNORE_NEW_LINES);
-    echo "File lines:\n";
-    foreach ($lines as $line) {
-        echo $line . "\n";
+
+        $lines = file($fileName, FILE_IGNORE_NEW_LINES);
+        if ($lines === false) {
+            echo "Error reading the file lines.<br>";
+        } else {
+            echo "File lines:\n";
+            foreach ($lines as $line) {
+                echo $line . "\n";
+            }
+        }
     }
 } else {
-    echo "File does not exist.\n";
+    echo "File does not exist.<br>";
     
+
     $initialContent = "This is the initial content of the file.\n";
-    file_put_contents($fileName, $initialContent);
-    echo "File created and content written.\n";
-    
-    $fileContents = file_get_contents($fileName);
-    echo "New file contents after creation:\n";
-    echo $fileContents . "\n";
+    $result = file_put_contents($fileName, $initialContent);
+    if ($result === false) {
+        echo "Error writing to the file.<br>";
+    } else {
+        echo "File created and content written.<br>";
+        
+
+        
+        if ($fileContents === false) {
+            echo "Error reading the file.<br>";
+        } else {
+            echo "New file contents after creation:\n";
+            echo $fileContents . "\n";
+        }
+    }
 }
 ?>
